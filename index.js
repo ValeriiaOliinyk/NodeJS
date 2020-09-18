4c5cc484c7406ce449d56cc09b22c287df03657c
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-09-18 12:33:29 +0300

    deleted few headers

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const app = express();
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
