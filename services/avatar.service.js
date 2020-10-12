87fb8997cc1ffd00e785e36a460a144027d8e88e
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-10-12 16:08:49 +0300

    added config.js

--- services/avatar.service.js
+++ services/avatar.service.js
@@ -1,16 +1,17 @@
 const AvatarGenerator = require("avatar-generator");
 const UserDB = require("../api/users/users.model");
+const { partsLocationGenerator, avatarPathGenerator } = require("./config");
 const path = require("path");
 
 const createAvatar = async (id) => {
   const avatar = new AvatarGenerator({
     parts: ["background", "face", "clothes", "head", "hair", "eye", "mouth"],
-    partsLocation: path.join(__dirname, "../img"),
+    partsLocation: partsLocationGenerator(),
     imageExtension: ".png",
   });
   const variant = "female";
   const image = await avatar.generate("email@example.com", variant);
-  image.png().toFile(`public/images/${id}.png`);
+  image.png().toFile(avatarPathGenerator(id));
 };
 
 const updateUserAvatar = async (id) => {
